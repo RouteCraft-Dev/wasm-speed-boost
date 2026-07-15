73e55738e19ed04723dfb9e3e38adfa6f48f5bc2
RouteCraft-Dev <routecraft.dev@gmail.com>
2026-07-14 21:38:00 -0300

    build: force upload compiled WASM pkg directory to fix 404

---
+++ pkg/wasm_speed_boost_bg.wasm.d.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const count_primes: (a: number) => number;
+export const is_prime: (a: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_start: () => void;
